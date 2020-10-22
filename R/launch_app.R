04e487da9626c33422650b1d8cacc90bdaaa11af
Zeyu <zyliu16@student.monash.edu>
2020-10-22 13:05:20 +0800

    fix bug

--- R/launch_app.R
+++ R/launch_app.R
@@ -7,9 +7,11 @@
 #' @param display.mode The mode in which to display the application. If set to the value "showcase", shows application code and metadata from a DESCRIPTION file in the application directory alongside the application. If set to "normal", displays the application normally. Defaults to "auto", which displays the application in the mode given in its DESCRIPTION file, if any.
 #' @param launch.browser If true, the system's default web browser will be launched automatically after the app is started. Defaults to true in interactive sessions only. This value of this parameter can also be a function to call with the application's URL.
 #'
-#' @examples 
+#' @examples
+#' \dontrun{ 
 #' launch_app()
 #' launch_app(display.mode = "showcase", launch.browser = TRUE)
+#' }
 #'
 #' @export
 #' 
